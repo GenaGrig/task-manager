88ab5ea2a9cf6454ea4b94af04c941745122d09b
GenaGrig <gennadiuss@gmail.com>
2023-06-16 16:29:08 +0000

    Fixed hamburger menu, now working

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,9 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let sidenav= document.querySelectorAll('.sidenav');
-    M.Sidenav.init(elems, options);
-  });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     let elems = document.querySelectorAll('.sidenav');
-//     let instances = M.Sidenav.init(elems, options);
-// });
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+});
