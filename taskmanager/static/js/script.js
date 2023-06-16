cf949d30ad4791f2d1e8028f9df15af08d23e11d
GenaGrig <gennadiuss@gmail.com>
2023-06-16 16:24:10 +0000

    Add: menu and core design

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav= document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, options);
+  });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     let elems = document.querySelectorAll('.sidenav');
+//     let instances = M.Sidenav.init(elems, options);
+// });
